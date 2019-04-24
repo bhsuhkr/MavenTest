dc591314fd0ab5d2bbfe03b057ebfd5d95f082ba
Bohyun Suh <bhsuhkr@gmail.com>
2019-04-24 00:56:14 -0500

    Phase2

---
+++ agent/src/main/java/edu/utdallas/MyClassFileTransform.java
@@ -0,0 +1,30 @@
+package edu.utdallas;
+import java.io.*;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+
+class MyClassFileTransform implements ClassFileTransformer {
+
+
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+        //if (className.startsWith("org/apache/commons/dbutils") || className.startsWith("org/joda/time")){
+		if (className.equals("FlipTableTest")) {
+         // if (className.startsWith("other")){
+            ClassReader cr = new ClassReader(classfileBuffer);
+            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+            MyJavaClassTransformVisitor ca = new MyJavaClassTransformVisitor(cw);
+            cr.accept(ca, 0);
+			System.out.format("\nHere 38 %s", className);
+            return cw.toByteArray();
+        }
+		
+        return classfileBuffer;
+        
+    }
+}
